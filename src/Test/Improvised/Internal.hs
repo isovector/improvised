59e00fd98c3b7084fc1b5a7c6f8a3e21eda123d7
Sandy Maguire <sandy@sandymaguire.me>
2020-02-04 16:49:07 +0700

    remove generics stuff

--- src/Test/Improvised/Internal.hs
+++ src/Test/Improvised/Internal.hs
@@ -9,36 +9,7 @@ module Test.Improvised.Internal where
 import Control.Monad.Trans.Reader
 import Control.Monad.Trans.Class
 import FCI.Internal
-import GHC.Generics
 import Data.Coerce
-
-
-class GCaptureInst i where
-  gcaptureInst :: i x
-
-instance GCaptureInst f => GCaptureInst (M1 _1 _2 f) where
-  gcaptureInst = M1 gcaptureInst
-
-instance GCaptureInst U1 where
-  gcaptureInst = U1
-
-instance (GCaptureInst f, GCaptureInst g) => GCaptureInst (f :*: g) where
-  gcaptureInst = gcaptureInst :*: gcaptureInst
-
-instance c => GCaptureInst (K1 _1 (Improvised c)) where
-  gcaptureInst = K1 inst
-
-
-class (Generic d, GCaptureInst (Rep d)) => CaptureInst d
-instance (Generic d, GCaptureInst (Rep d)) => CaptureInst d
-
-captureInst :: CaptureInst d => d
-captureInst = to gcaptureInst
-
-
-coerceImprovisable :: Improvisable dict m a -> dict -> m a
-coerceImprovisable = coerce
-
 
 
 newtype Improvisable dict m a = Improvisable (ReaderT dict m a)
@@ -46,11 +17,15 @@ newtype Improvisable dict m a = Improvisable (ReaderT dict m a)
 
 instance MonadTrans (Improvisable dict) where
   lift = Improvisable . lift
+  {-# INLINABLE lift #-}
 
 
 improvise :: dict -> Improvisable dict m a -> m a
 improvise dict (Improvisable r) = runReaderT r dict
+{-# INLINABLE improvise #-}
 
-runImprovisable :: CaptureInst dict => Improvisable dict m a -> m a
-runImprovisable (Improvisable r) = runReaderT r captureInst
+
+coerceImprovisable :: Improvisable dict m a -> dict -> m a
+coerceImprovisable = coerce
+{-# INLINABLE coerceImprovisable #-}
 
